1299fc8c2a6c84117c10ee8258ed4e06a1ea36d0
M4TTH3 <matthewau-yeung@hotmail.com>
2024-01-07 00:21:09 -0500

    now host azure functions on a vm

--- sw-azurefn/function_app.py
+++ sw-azurefn/function_app.py
@@ -6,9 +6,6 @@ from routes.session import bp as session
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-# app.register_functions(getuser)
-# app.register_functions(setuser)
-
 app.register_functions(friends)
 app.register_functions(user)
-# app.register_functions(session)
+app.register_functions(session)
